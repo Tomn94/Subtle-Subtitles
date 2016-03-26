6e3c14058780ef33dec9e173a54dcfa4421092a5
Tomn <tmnstudios@hotmail.fr>
2016-03-26 21:25:51 +0100

    Initial Commit

---
+++ Subtle Subtitles/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  Subtle Subtitles
+//
+//  Created by Tomn on 26/03/2016.
+//  Copyright © 2016 Tomn. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ Subtle Subtitles/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Subtle Subtitles
+//
+//  Created by Tomn on 26/03/2016.
+//  Copyright © 2016 Tomn. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
